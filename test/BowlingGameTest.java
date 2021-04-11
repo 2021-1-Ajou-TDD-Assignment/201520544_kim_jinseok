36bb4725c3dda1e33f4bbc9aab1113f7ce44cc67
Jin <ekfaktldk11@ajou.ac.kr>
2021-04-12 01:09:51 +0900

    The Second test

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -12,12 +12,17 @@ public class BowlingGameTest {
 	}
 	@Test
 	public void testGutterGame() {
-		for (int i = 0; i < 20; i++) g.roll(0);
+		rollMany(20, 0);
 		assertEquals(0,g.score());
+	}
+	private void rollMany(int n, int pins) {
+		for (int i = 0; i < n; i++) {
+			g.roll(pins);
+		}
 	}
 	@Test
 	public void testAllOnews() {
-		for (int i = 0; i<20; i++) g.roll(1);
+		rollMany(20, 1);
 		assertEquals(20,g.score());
 	}
 }
